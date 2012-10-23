a733d59bf10da266c06c33302efe085e5bc53eae
Young Soo Song <extramorein@gmail.com>
2012-10-23 14:13:36 +0900

    New file, histNtabke.R was created.
    To creat overlapping histogram and table for data received from Chung
    team manager

---
+++ histNtable.R
@@ -0,0 +1,8 @@
+function(x) {
+h1 <- hist(d[,x], breaks=seq(0,40, by=1))
+h2 <- hist(e[,x], breaks=seq(0,40, by=1))
+plot(h1, col=rgb(0, 0, 1, 1/4))
+plot(h2, col=rgb(0, 0, 1, 1/4), add=T)
+table(d[,x])
+table(e[,x])
+}
