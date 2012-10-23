0c4b2453ef4ad55c295aba62da1f29fc3e7d1a51
Young Soo Song <extramorein@gmail.com>
2012-10-23 15:44:06 +0900

    try to debug

--- hNt.R
+++ hNt.R
@@ -1,16 +1,3 @@
-histNtable <- function(x) {
-x <- 3
-return x
-
-#d <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Small Cell Lung Cancer ¨¬¨¢¨ù¢ç\\HYUniv.LSC.WGS.10Pairs.Gene_summary.csv")
-#e <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Breast Cancer ¨¬¨¢¨ù¢ç\\BreastCancer.gene_sorting.csv") 
-
-#h1 <- hist(d[,x], breaks=seq(0,40, by=1))
-#h2 <- hist(e[,x], breaks=seq(0,40, by=1))
-#plot(h1, col=rgb(0, 0, 1, 1/4))
-#plot(h2, col=rgb(0, 0, 1, 1/4), add=T)
-#cat(table(d[,x]))
-#cat("\n")
-#cat(table(e[,x]))
-#cat("\n")
+hNt <- function(x) {
+  x <- 3
 }
