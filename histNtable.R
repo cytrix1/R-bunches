79b675af4c6d9d6c8c1ce35efdfd9ca6e3fca79b
Young Soo Song <extramorein@gmail.com>
2012-10-23 15:39:47 +0900

    try to debug

--- histNtable.R
+++ histNtable.R
@@ -1,9 +1,12 @@
 histNtable <- function(x) {
-d <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Small Cell Lung Cancer 분석\\HYUniv.LSC.WGS.10Pairs.Gene_summary.csv")
-e <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Breast Cancer 분석\\BreastCancer.gene_sorting.csv") 
+x <- 3
+return x
 
-h1 <- hist(d[,x], breaks=seq(0,40, by=1))
-h2 <- hist(e[,x], breaks=seq(0,40, by=1))
+#d <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Small Cell Lung Cancer 분석\\HYUniv.LSC.WGS.10Pairs.Gene_summary.csv")
+#e <- read.csv("C:\\Users\\nam\\Dropbox\\Research projects\\Breast Cancer 분석\\BreastCancer.gene_sorting.csv") 
+
+#h1 <- hist(d[,x], breaks=seq(0,40, by=1))
+#h2 <- hist(e[,x], breaks=seq(0,40, by=1))
 #plot(h1, col=rgb(0, 0, 1, 1/4))
 #plot(h2, col=rgb(0, 0, 1, 1/4), add=T)
 #cat(table(d[,x]))
