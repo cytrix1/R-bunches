95932fab7f5ba1a7c90f5038a8e6639c86f272f9
Young Soo Song <extramorein@gmail.com>
2012-10-23 15:47:09 +0900

    ttt

--- hNt.R
+++ hNt.R
@@ -1,3 +1,14 @@
-hNt <- function(x) {
-  x <- 3
+mysummary <- function(x,npar=TRUE,print=TRUE) {
+  if (!npar) {
+    center <- mean(x); spread <- sd(x) 
+  } else {
+    center <- median(x); spread <- mad(x) 
+  }
+  if (print & !npar) {
+    cat("Mean=", center, "\n", "SD=", spread, "\n")
+  } else if (print & npar) {
+    cat("Median=", center, "\n", "MAD=", spread, "\n")
+  }
+  result <- list(center=center,spread=spread)
+  return(result)
 }
