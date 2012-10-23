c18698251684349b7f94a414be658934797dca0f
Young Soo Song <extramorein@gmail.com>
2012-10-23 16:04:13 +0900

    ; added

--- tet.R
+++ tet.R
@@ -1,5 +1,4 @@
-histNtable <- function(x) {
-h1 <- hist(d[,x], breaks=seq(0,40, by=1))
+histNtable <- function(x) { h1 <- hist(d[,x], breaks=seq(0,40, by=1))
 h2 <- hist(e[,x], breaks=seq(0,40, by=1))
 plot(h1, col=rgb(0, 0, 1, 1/4))
 plot(h2, col=rgb(0, 0, 1, 1/4), add=T)
